c1c7174484d1ced07445c9b8a8cd7dcd07144a94
DevETB <nnicrodu@etb.com.co>
2024-11-20 17:52:18 -0500

    Se ha realizado la subida del simulador SALESFORCE

---
+++ SALESFORCE/Controller/case/chat.js
@@ -0,0 +1,17 @@
+function change2() {
+    $('#ases2').css('display', 'block');  
+    $('#clie2').css('display', 'block');  
+    $('#chat2').css('display', 'none');
+    $('#chat3').css('display', 'block');      
+}
+
+function change3() {
+    $('#ases3').css('display', 'block');  
+    $('#clie3').css('display', 'block');   
+    $('#chat3').css('display', 'none');
+    $('#chat4').css('display', 'block');          
+}
+
+function change4() {
+    $('#ases4').css('display', 'block');           
+}
---
+++ SALESFORCE/View/components/topBar.php
@@ -0,0 +1,33 @@
+
+        <div class="container">
+
+            <img src="../images/icons/logo.png" alt="">
+
+            <input type="text" placeholder="Buscar...">
+
+            <div class="icons">
+                <img src="../images/icons/star.png" alt="">
+                <img src="../images/icons/plus.png" alt="">
+                <img src="../images/icons/got.png" alt="">
+                <img src="../images/icons/question.png" alt="">
+                <img src="../images/icons/bell.png" alt="">
+                <img src="../images/icons/user.png" alt="">
+            </div>
+
+        </div>
+
+        <div class="line"></div>
+
+        <div class="container-2">
+
+            <img src="../images/icons/menu.png" alt="">
+
+            <p id="title">Consola de Servicios</p>
+
+            <p id="par">Casos</p>
+
+            <div id="arrow">
+                <img src="../images/icons/fot arrow.png" alt="">
+            </div>
+
+        </div>
