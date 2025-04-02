de5a7b91ec1a05fecb074854ab7ac37f4e49502f
DevETB <nicolasrodriguez100522@outlook.com>
2025-04-02 09:43:45 -0500

    Se ha dado de baja al simulador por problemas con los commits

--- SUMA/View/components/infoAudios.php
+++
@@ -1,8 +0,0 @@
-<div class="con-info-audios">
-            <div class="con-img-au">
-                <img src="../../../images/speaker2.png" alt="parlante-audio-etb" class="img-audio" id="no-audio-f">
-            </div>
-            <div class="info-audios">
-                <p>Presiona sobre el icono para escuchar las indicaciones ;)</p>
-            </div>
-</div>
