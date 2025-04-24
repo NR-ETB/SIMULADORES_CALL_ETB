f89da9a7f22ac2215759f6ea90193b8bf7fab931
DevETB <nicolasrodriguez100522@outlook.com>
2025-04-24 11:27:07 -0500

    Se ha eliminado temporalmente el proyecto

--- SUMA/Controller/suma/FALLA/portalFALLA2.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $('#modal-loading').modal('toggle')
-    });
-    setTimeout(() => {
-    $( document ).ready(function() {
-        $('#modal-loading').modal('hide')
-        $('#modal-fase1').modal('toggle') 
-    });
-}, "4000");
